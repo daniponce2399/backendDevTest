43c2eca4d6394c2adb1a3cb34c9b7def91e61c5a
daniponce2399 <47412902+daniponce2399@users.noreply.github.com>
2022-10-29 20:44:37 +0200

    Add files via upload

---
+++ src/src/main/java/shop/BackEndDev.java
@@ -0,0 +1,15 @@
+package shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Method to start the application
+ *
+ */
+@SpringBootApplication
+public class BackEndDev {
+
+    public static void main( String[] args ){SpringApplication.run(shop.BackEndDev.class, args);}
+
+}
---
+++ src/src/test/java/shop/BackEndDevTest.java
@@ -0,0 +1,16 @@
+package shop;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+
+@SpringBootTest
+public class BackEndDevTest {
+
+    @Test
+    public void  contextLoad(){
+
+    }
+
+}
